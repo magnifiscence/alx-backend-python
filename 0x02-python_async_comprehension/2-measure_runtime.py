66caa2b4b9b17b424ae76782b68d95e8180d560b
tata <tdivine284@gmail.com>
2024-03-20 00:28:34 +0000

    task done

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the runtime"""
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.perf_counter()
+    return end - start
